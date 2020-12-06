00f5f41a2775af6330b2fd10469b51b3e7a4d287
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-06 16:39:56 +0100

    More progress

--- 2020/5/index.js
+++ 2020/5/index.js
@@ -1,3 +1,51 @@
 const fs = require('fs');
 
 const inputList = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+const seatIds = inputList.map((seat) => {
+  // Min and max should be equal at the end so we only need one of them
+  const { minRow, minCol } = seat.split('').reduce((acc, code) => {
+    const rowIncrement = Math.ceil((acc.maxRow - acc.minRow)/ 2);
+    const colIncrement = Math.ceil((acc.maxCol - acc.minCol)/ 2);
+
+    switch (code) {
+      case 'F':
+        return {
+          ...acc,
+          maxRow: acc.maxRow - rowIncrement
+        };
+      case 'B':
+        return {
+          ...acc,
+          minRow: acc.minRow + rowIncrement
+        };
+      case 'L':
+        return {
+          ...acc,
+          maxCol: acc.maxCol - colIncrement
+        };
+      case 'R':
+        return {
+          ...acc,
+          minCol: acc.minCol + colIncrement
+        };
+    }
+  }, { minRow: 0, maxRow: 127, minCol: 0, maxCol: 7 });
+
+  return (minRow * 8) + minCol;
+});
+
+const getHighestSeatId = () => seatIds.reduce((a, b) => Math.max(a, b));
+
+const getMissingSeatId = () => {
+  // Don't modify the original array, just in case
+  const sortedSeatIds = [...seatIds].sort();
+  const startingSeatId = sortedSeatIds[0];
+
+  // Numbers should appear one after another in order
+  // If we skipped a number at some point, then the missing one is the one immediately before it
+  return sortedSeatIds.find((seatId, idx) => seatId !== startingSeatId + idx) - 1;
+};
+
+console.log('Part 1: ', getHighestSeatId());
+console.log('Part 2: ', getMissingSeatId());
---
+++ 2020/6/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const groupsList = fs.readFileSync('input.txt', 'utf8').split('\n\n');
+
+const getTotalYesCount = () => {
+  const answersPerGroup = groupsList.map((group) => {
+    const allYes = group.replace(/\n/g, '');
+    return Array.from(new Set(allYes)).length;
+  });
+
+  return answersPerGroup.reduce((acc, numYes) => acc += numYes, 0)
+}
+
+const getTotalYesQuestionsCount = () => {
+  const sharedAnswers = groupsList.map((group) => {
+    // Get unique questions for each person
+    const yesPerPerson = group.split('\n').map((yes) => Array.from(new Set(yes)));
+    // Multi-array intersection: get questions everyone answered yes to
+    // Not the most efficient, but it works
+    return yesPerPerson.reduce((a, b) => a.filter(c => b.includes(c))).length;
+  });
+
+  return sharedAnswers.reduce((acc, numYes) => acc += numYes, 0)
+}
+
+console.log('Part 1: ', getTotalYesCount());
+console.log('Part 2: ', getTotalYesQuestionsCount());
