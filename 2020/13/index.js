8043e0211ec2b8986c6314411171b02c6c39433c
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-20 16:52:49 +0100

    13.1

--- 2020/13/index.js
+++ 2020/13/index.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
 
-const inputLines = fs.readFileSync('input.txt', 'utf8').split('\n');
+const [timestamp, schedules] = fs.readFileSync('input.txt', 'utf8').split('\n');
+const runningBusses = schedules.split(',').filter((bus) => bus !== 'x').map((x) => +x);
 
+const getWaitTimes = () => {
+  const departureTime = +timestamp;
+  const firstBus = runningBusses.reduce((acc, busId) => {
+    const waitTime = busId - (departureTime % busId);
+    return waitTime < acc.waitTime ? { busId, waitTime } : acc;
+  }, { busId: 0, waitTime: Number.MAX_SAFE_INTEGER });
+
+  return firstBus.busId * firstBus.waitTime;
+}
+
+console.log('Part 1: ', getWaitTimes());
