cd1e8158dbb14672384cc864c0e393f46ec1ae7c
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-25 21:29:23 +0100

    14.2

--- 2020/14/index.js
+++ 2020/14/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const inputLines = fs.readFileSync('input.txt', 'utf8').split('\n');
 
-const applyMask = (mask, value) => {
+const applyMask = (mask, value, unchangedBit) => {
   // Value is a base 10 number, convert it to binary and
   // pad with leading 0s so it has length of 36
   let binaryValue = (+value).toString(2);
@@ -12,28 +12,65 @@ const applyMask = (mask, value) => {
 
   let maskedVal = '';
   for (let i = 0; i < 36; i++) {
-    const nextBit = mask[i] === 'X' ? binaryValue[i] : mask[i];
+    const nextBit = mask[i] === unchangedBit ? binaryValue[i] : mask[i];
     maskedVal = maskedVal + nextBit;
   }
 
   return maskedVal;
 }
 
-const getMemorySums = () => {
+const applyMaskWithFloats = (mask, memAddr, value) => {
+  const maskedAddr = applyMask(mask, memAddr, '0');
+  const binaryVal = (+value).toString(2);
+  const numFloats = (maskedAddr.match(/X/g)).length;
+
+  if (!numFloats) {
+    // No floats, only need to write to one memory address
+    return {
+      [parseInt(maskedAddr, 2)]: binaryVal,
+    }
+  }
+
+  const allAddrs = maskedAddr.split('').reduce((addrList, bit, idx) => {
+    if (bit !== 'X') return addrList;
+
+    return addrList.reduce((acc, inProgAddr) =>
+      [
+        ...acc,
+        inProgAddr.substring(0, idx) + '0' + inProgAddr.substring(idx + 1),
+        inProgAddr.substring(0, idx) + '1' + inProgAddr.substring(idx + 1),
+      ], []);
+  }, [maskedAddr]);
+
+  return allAddrs.reduce((acc, addr) => ({
+    ...acc,
+    [parseInt(addr, 2)]: binaryVal,
+  }), {});
+}
+
+const getMemorySums = (isV2 = false) => {
   let currentMask;
   const memoryMap = inputLines.reduce((memMap, line) => {
     const [instr, value] = line.split(' = ');
     if (instr === 'mask') {
       currentMask = value;
       return memMap;
-    } else {
-      // mem lines always come in this form: "mem[xxxx]"
-      const key = instr.slice(4, instr.length - 1);
-      const newVal = applyMask(currentMask, value);
+    }
+    
+    // mem lines always come in this form: "mem[xxxx]"
+    const key = instr.slice(4, instr.length - 1);
+
+    if (!isV2) {
+      const newVal = applyMask(currentMask, value, 'X');
       return {
         ...memMap,
         [key]: newVal,
       }
+    }
+
+    return {
+      ...memMap,
+      ...applyMaskWithFloats(currentMask, key, value),
     }
   }, {});
 
@@ -41,3 +78,4 @@ const getMemorySums = () => {
 }
 
 console.log('Part 1: ', getMemorySums());
+console.log('Part 2: ', getMemorySums(true));
---
+++ 2020/15/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8').split(',').map(Number);
+
+const getNumberSpoken = (iter) => {
+
+}
+
+console.log('Part 1: ', getNumberSpoken(2020));
