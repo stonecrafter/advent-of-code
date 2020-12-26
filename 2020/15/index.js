7d5b6ac444ce8281f1390d79fdca3a3878c3a3c0
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-26 16:51:05 +0100

    15.1

--- 2020/15/index.js
+++ 2020/15/index.js
@@ -3,7 +3,38 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8').split(',').map(Number);
 
 const getNumberSpoken = (iter) => {
+  const numAndTurns = {};
+  let mostRecentNum;
 
+  for (let i = 1; i <= iter; i++) {
+    // Go through starting numbers first
+    // The list of numbers is always unique
+    if (i <= input.length) {
+      mostRecentNum = input[i - 1];
+      numAndTurns[mostRecentNum] = [i];
+    } else {
+      // Always at least length of 1, since this is the
+      // previously spoken number
+      const turnsMap = numAndTurns[mostRecentNum];
+
+      // If the number has been spoken only once,
+      // next number is 0. If it has been spoken at least twice,
+      // take the difference of those two turn numbers.
+      // The most recent turn number comes in the beginning.
+      const newNum = (turnsMap.length === 1) ? 0 : turnsMap[0] - turnsMap[1];
+      const newTurnsMap = numAndTurns[newNum];
+
+      // Add the current number to the start of the turns map
+      // Or start a new array if this number has never been spoken before
+      numAndTurns[newNum] = !!newTurnsMap ? [i, ...newTurnsMap] : [i];
+
+      // Now this becomes the most recently spoken number
+      mostRecentNum = newNum;
+    }
+  }
+
+  return mostRecentNum;
 }
 
 console.log('Part 1: ', getNumberSpoken(2020));
+console.log('Part 2: ', getNumberSpoken(30000000));
