46b7476b22e1bc01a0aa8efcf4a182e753dfcb4d
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-26 20:36:40 +0100

    Start 17

---
+++ 2020/17/index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const inputLines = fs.readFileSync('input.txt', 'utf8').split('\n');
+
+const getActiveCubes = () => {
+  
+}
+
+console.log('Part 1: ', getActiveCubes());
