45b2869737811a135994c64982137262d4112aa4
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-26 17:19:17 +0100

    15.2

--- 2020/15/index.js
+++ 2020/15/index.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const input = fs.readFileSync('input.txt', 'utf8').split(',').map(Number);
 
 const getNumberSpoken = (iter) => {
-  const numAndTurns = {};
+  // Turns out using Map is way faster than object
+  const numAndTurns = new Map();
   let mostRecentNum;
 
   for (let i = 1; i <= iter; i++) {
@@ -11,22 +12,23 @@ const getNumberSpoken = (iter) => {
     // The list of numbers is always unique
     if (i <= input.length) {
       mostRecentNum = input[i - 1];
-      numAndTurns[mostRecentNum] = [i];
+      numAndTurns.set(mostRecentNum, [i]);
     } else {
       // Always at least length of 1, since this is the
       // previously spoken number
-      const turnsMap = numAndTurns[mostRecentNum];
+      const turnsMap = numAndTurns.get(mostRecentNum);
 
       // If the number has been spoken only once,
       // next number is 0. If it has been spoken at least twice,
       // take the difference of those two turn numbers.
       // The most recent turn number comes in the beginning.
       const newNum = (turnsMap.length === 1) ? 0 : turnsMap[0] - turnsMap[1];
-      const newTurnsMap = numAndTurns[newNum];
+      const newTurnsMap = numAndTurns.get(newNum);
 
       // Add the current number to the start of the turns map
       // Or start a new array if this number has never been spoken before
-      numAndTurns[newNum] = !!newTurnsMap ? [i, ...newTurnsMap] : [i];
+      const newArray = !!newTurnsMap ? [i, newTurnsMap[0]] : [i];
+      numAndTurns.set(newNum, newArray);
 
       // Now this becomes the most recently spoken number
       mostRecentNum = newNum;
