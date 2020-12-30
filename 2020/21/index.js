23fe7f1d7a8bddb2c76ec4ccf00ef0b4194c4d08
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-30 10:27:43 +0100

    21.2

--- 2020/21/index.js
+++ 2020/21/index.js
@@ -36,12 +36,12 @@ const { mapByAllergen, listByFood, mapByIngredient } = inputList.reduce((acc, fo
 const countOccurrences = (arr, val) =>
   arr.reduce((acc, item) => item === val ? acc + 1 : acc, 0);
 
-const getAllergenCount = () => {
-  // Go through the list of allergens, and inside each allergen,
-  // look at every item. If the count of that item is equal
-  // to the number of foods listed as containing that allergen,
-  // that ingredient is potentially unsafe
+const getUnsafeItems = () => {
   const unsafeItems = Object.keys(mapByAllergen).reduce((acc, allergen) => {
+    // Go through the list of allergens, and inside each allergen,
+    // look at every item. If the count of that item is equal
+    // to the number of foods listed as containing that allergen,
+    // that ingredient is potentially unsafe
     const allergenList = mapByAllergen[allergen];
     const notThisAllergen = allergenList.filter((item) => {
       const itemCountInList = countOccurrences(allergenList, item);
@@ -52,7 +52,13 @@ const getAllergenCount = () => {
     return [...acc, ...notThisAllergen];
   }, []);
 
+  // Remove duplicates
+  return Array.from(new Set(unsafeItems));
+}
+
+const getAllergenCount = () => {
   // All foods that are not in the unsafe list, is safe
+  const unsafeItems = getUnsafeItems();
   const safeItems = Object.keys(mapByIngredient).reduce((acc, item) =>
     unsafeItems.includes(item) ? acc : [...acc, item], []);
 
@@ -60,4 +66,56 @@ const getAllergenCount = () => {
   return safeItems.reduce((acc, item) => acc + mapByIngredient[item].length, 0);
 }
 
+const getAllergenList = () => {
+  const unsafeItems = getUnsafeItems();
+  // Keep track of which ingredients are potentially still allergens
+  const potentialAllergens = Object.keys(mapByAllergen).reduce((acc, key) => {
+    const items = Array.from(new Set(mapByAllergen[key].filter((item) => unsafeItems.includes(item))))
+    return {
+      ...acc,
+      [key]: items
+    };
+  }, {});
+
+  // Do all items containing this allergen, also contain this ingredient?
+  // This nested loop hurts a little :D
+  Object.keys(mapByAllergen).forEach((allergen) => {
+    unsafeItems.forEach((unsafeItem) => {
+      listByFood.forEach((food) => {
+        if (food.allergens.includes(allergen) && !food.items.includes(unsafeItem)) {
+          // Remove this item as a potential candidate for this allergen
+          potentialAllergens[allergen] =
+            potentialAllergens[allergen].filter((i) => i !== unsafeItem);
+        }
+      });
+    });
+  });
+
+  // Keep going until every allergen maps to exactly one ingredient
+  // This is guaranteed to be possible in the puzzle input
+  while (Object.values(potentialAllergens).some((arr) => arr.length > 1)) {
+    // If an allergen maps to only one ingredient, remove that ingredient
+    // from the potential list of all other allergens
+    Object.keys(potentialAllergens).forEach((allergen) => {
+      const potentialItems = potentialAllergens[allergen];
+      if (potentialItems.length === 1) {
+
+        Object.keys(potentialAllergens).forEach((otherAllergen) => {
+          if (otherAllergen !== allergen) {
+            potentialAllergens[otherAllergen] =
+              potentialAllergens[otherAllergen].filter((i) => i !== potentialItems[0]);
+          }
+        });
+      }
+    });
+  }
+
+  // Get alphabetically sorted list of ingredients
+  return Object.keys(potentialAllergens)
+    .sort((a, b) => a.localeCompare(b))
+    .map((sortedAllergen) => potentialAllergens[sortedAllergen])
+    .join(',');
+}
+
 console.log('Part 1: ', getAllergenCount());
+console.log('Part 2: ', getAllergenList());
