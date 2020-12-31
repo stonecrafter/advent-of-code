2a7c365fd9d5d25ccee97fe7d00a14a60a18cffc
Kirien Eyma <kirien.eyma@cognite.com>
2020-12-31 23:25:36 +0100

    25.1

---
+++ 2020/25/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+
+const [cardPubKey, doorPubKey] = fs.readFileSync('input.txt', 'utf8').split('\n').map(Number);
+
+const SUBJECT_NUM = 7;
+
+const getLoopSize = (pubKey) => {
+  let val = 1;
+  let loopSize = 0;
+  while (val !== pubKey) {
+    val = (val * SUBJECT_NUM) % 20201227;
+    loopSize += 1;
+  }
+
+  return loopSize;
+}
+
+const getKeyForLoopSize = (loopSize, subjectNum) => {
+  let val = 1;
+  let currentLoop = 0;
+  while (currentLoop < loopSize) {
+    val = (val * subjectNum) % 20201227;
+    currentLoop += 1;
+  }
+
+  return val;
+}
+
+const getEncryptionKey = () =>
+  getKeyForLoopSize(getLoopSize(cardPubKey), doorPubKey);
+
+console.log('Part 1: ', getEncryptionKey());
